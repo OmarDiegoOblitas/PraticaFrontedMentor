cc4c39b93b4aa833df3c27b118666351884e794d
OmarDiegoOblitas <51056602+OmarDiegoOblitas@users.noreply.github.com>
2021-11-01 15:38:02 -0500

    Practica de frontedMentor

---
+++ images/main.js
@@ -0,0 +1,8 @@
+// Grap Dom Elements//
+const shareIcon = document.querySelector(".share__icon")
+const tooltip = document.querySelector(".tooltip")
+
+//Listen for click events//
+shareIcon.addEventListener("click", () => {
+    tooltip.classList.toggle("active")
+})
